6f8689907d35b400a44aa6b107b5a130e9777752
abiFarhan <abifarhan04@gmail.com>
2021-12-13 11:19:30 +0700

    Challenge 1 -> Completed

--- app/src/main/java/com/abifarhan/mybizcard/MainActivity.kt
+++ app/src/main/java/com/abifarhan/mybizcard/MainActivity.kt
@@ -3,18 +3,25 @@ package com.abifarhan.mybizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.abifarhan.mybizcard.ui.theme.MyBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,21 +41,72 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Text(text  = "BizCard")
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
-            .height(390.dp)
-            .padding(12.dp),
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             backgroundColor = Color.White,
             elevation = 4.dp
         ) {
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider(
+                    startIndent = 123.dp
+                )
+                
+                Text(text = "Miles P. +",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    fontSize = 30.sp,
+                    color = Color.Blue
+                )
+                Text(text = "Android Compose Programmer")
+                Text(text = "@themilesCompose")
 
+                Button(
+                    onClick = { /*TODO*/ },
+                    Modifier.padding(10.dp),
+                ) {
+
+                    Text(text = "Portfolio")
+                }
+            }
+                        
         }
     }
 }
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp, 150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_person_24),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
